104917518a5639c86a6a43b3d344124b853ae5b8
Inbar Copitman <inbarcopitman@gmail.com>
2021-02-09 03:28:32 +0200

    - Added spaces.

--- ASP.NET_Final_Project/Controllers/DepartmentsController.cs
+++ ASP.NET_Final_Project/Controllers/DepartmentsController.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using System.Linq;
 using ASP.NET_Final_Project.Data;
 using ASP.NET_Final_Project.Models;
@@ -14,49 +13,52 @@ namespace ASP.NET_Final_Project.Controllers
         {
             _db = db;
         }
-        
+
         public IActionResult Index()
         {
             ViewBag.departments = _db.Departments.ToList(); // TODO get relative to user id
+
             return View();
         }
-        
+
         public IActionResult Add()
         {
             return View();
         }
-        
+
         [HttpPost]
         public ActionResult AddDepartment(Department dep)
         {
             dep.UserId = 1; // TODO change to user id from session.
             _db.Departments.Add(dep);
             _db.SaveChanges();
-            
+
             return RedirectToAction("Index");
         }
-        
+
         public IActionResult Edit(int Id)
         {
-            Department dep = _db.Departments.First(x => x.Id == Id);
+            var dep = _db.Departments.First(x => x.Id == Id);
+
             return View("Edit", dep);
         }
-        
+
         [HttpPost]
         public ActionResult UpdateDepartment(Department dep)
         {
             var department = _db.Departments.FirstOrDefault(x => x.Id == dep.Id);
             if (department != null) department.Name = dep.Name;
             _db.SaveChanges();
-            
+
             return RedirectToAction("Index");
         }
-        
+
         public IActionResult Delete(int Id)
         {
             var department = _db.Departments.First(x => x.Id == Id);
-            if(department != null) _db.Departments.Remove(department);
+            if (department != null) _db.Departments.Remove(department);
             _db.SaveChanges();
+
             return RedirectToAction("Index");
         }
     }
